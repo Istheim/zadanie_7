e9503bbc3ddd8d6c0c7e3213ffd0dc30788058de
Matvey Zaytsev(Istheim) <zaytsev_matvey98@mail.ru>
2023-10-06 14:59:46 +0700

    4 задание

--- user/serliazers.py
+++ user/serliazers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from payment.serliazers import PaymentSerializer
 from user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(source='payment_set', many=True)  # Сериализатор для истории платежей
+
     class Meta:
         model = User
         fields = '__all__'
